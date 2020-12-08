a2804715a13e109c2ee9cf1c9c050dd98adb4c8f
savagecarol <karthiksharma1411@gmail.com>
2020-12-08 16:14:45 +0530

    avatar added on about page

--- lib/about.dart
+++ lib/about.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:c_syntax/contributors.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:link/link.dart';
@@ -10,13 +9,11 @@ class About extends StatefulWidget {
 }
 
 class _AboutState extends State<About> {
-
-
   List<String> names = [];
   List<String> urls = [];
+  List<String> images = [];
+
   bool isLoading = true;
-
-
 
   @override
   void initState() {
@@ -24,10 +21,11 @@ class _AboutState extends State<About> {
     getUsername();
   }
 
-
   void getUsername() async {
     List<String> nameList = [];
     List<String> urlList = [];
+    List<String> imagesList = [];
+
     Response response = await get(
         'https://api.github.com/repos/enzaimz/c_made_easy/contributors');
     final result = jsonDecode(response.body);
@@ -35,38 +33,45 @@ class _AboutState extends State<About> {
     for (var map in result) {
       nameList.add(map['login']);
       urlList.add(map['html_url']);
+      imagesList.add(map['avatar_url']);
     }
 
     setState(() {
       isLoading = false;
       names = nameList;
       urls = urlList;
+      images = imagesList;
     });
   }
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('About Developers'),
       ),
-     body: isLoading
+      body: isLoading
           ? Center(child: CircularProgressIndicator())
           : ListView.builder(
               itemCount: names.length,
               itemBuilder: (context, index) {
                 return Card(
-  
                   child: Container(
                     padding: EdgeInsets.all(8.0),
                     child: Link(
-                        url: urls[index],
-                            child: ListTile(
-                          leading: Icon(Icons.account_circle),
-                          title:  Text(names[index],
+                      url: urls[index],
+                      child: ListTile(
+                        leading: CircleAvatar(
+                radius: 20.0,
+                backgroundImage:
+                    NetworkImage(images[index]),
+                backgroundColor: Colors.transparent,
+              )
+,
+                        title: Text(
+                          names[index],
+                        ),
                       ),
-            ),
                     ),
                   ),
                 );
