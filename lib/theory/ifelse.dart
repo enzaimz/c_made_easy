eb60888d2764352e57ae316d31ef5edfb401d5bf
amitpola <61548277+amitpola@users.noreply.github.com>
2020-12-12 19:38:50 +0530

    Update ifelse.dart

--- lib/theory/ifelse.dart
+++ lib/theory/ifelse.dart
@@ -10,13 +10,13 @@ class Ifelse extends StatelessWidget{
   String ifelseDes='\nIf the test expression is evaluated to true, statements inside the body of if are executed and statements inside the body of else are skipped from execution.\n\nIf the test expression is evaluated to false,statements inside the body of else are executed statements inside the body of if are skipped from execution.\n ';
   String ifelseSnippet='if ( test Expression )\n{\n\t\t\t\t// statements to be executed if the \n\t\t\t\ttest expression is true\n}\n\nelse\n{\n\t\t\t\t// statements to be executed if the \n\t\t\t\ttest expression is false\n}';
 
-  String NifStatement='\nNested if else';
-  String NifelseDes='\nIf the test expression 1 is evaluated to true,statements inside the body of outer if are executed,followed by the inner if statement.\n\nIf the test expression 2 of inner if statement evaluates to true,the statements inside it are executed. Statements inside the body of outer and inner else are skipped from execution.\n\nIf the test expression 2 is evaluated to false,statements inside the body of inner else are executed and statements inside the body of inner else and  outer else are skipped from execution.\n\nIf the test expression 1 evaluates to false then both inner if and else statements are skipped and only outer else statements are evaluated.  ';
-  String NifelseSnippet='if ( test Expression 1 )\n{\n\t\t\t\tif( test expression 2 )\n\t\t\t\t{\n\t\t\t\t// statements under inner if to be\n\t\t\t\t executed.\n\t\t\t\t}\n\n\t\t\t\telse\n\t\t\t\t{\n\t\t\t\t//statements to be executed if the test \n\t\t\t\t expression 2 evaluates to false.\n\t\t\t\t}\n}\n\nelse\n{\n\t\t\t// statements to be executed if the \n\t\t\t\ttest expression 1 is false.\n}';
+  String nifStatement='\nNested if else';
+  String nifelseDes='\nIf the test expression 1 is evaluated to true,statements inside the body of outer if are executed,followed by the inner if statement.\n\nIf the test expression 2 of inner if statement evaluates to true,the statements inside it are executed. Statements inside the body of outer and inner else are skipped from execution.\n\nIf the test expression 2 is evaluated to false,statements inside the body of inner else are executed and statements inside the body of inner else and  outer else are skipped from execution.\n\nIf the test expression 1 evaluates to false then both inner if and else statements are skipped and only outer else statements are evaluated.  ';
+  String nifelseSnippet='if ( test Expression 1 )\n{\n\t\t\t\tif( test expression 2 )\n\t\t\t\t{\n\t\t\t\t// statements under inner if to be\n\t\t\t\t executed.\n\t\t\t\t}\n\n\t\t\t\telse\n\t\t\t\t{\n\t\t\t\t//statements to be executed if the test \n\t\t\t\t expression 2 evaluates to false.\n\t\t\t\t}\n}\n\nelse\n{\n\t\t\t// statements to be executed if the \n\t\t\t\ttest expression 1 is false.\n}';
 
-  String LifStatement='\nif-else Ladder ';
-  String LifelseDes='\nAs soon as one of the conditions controlling the if is true, the statement associated with that if is executed, and the rest of the C else-if ladder is bypassed.\n\nIf none of the conditions are true, then the final else statement will be executed.';
-  String LifelseSnippet='if ( condition )\n\t\t\t//statements to be executed;\n\nelse if ( condition )\n\t\t\t//statements to be executed;\n.\n.\n.\nelse\n\t\t\t//statements to be executed; ';
+  String lifStatement='\nif-else Ladder ';
+  String lifelseDes='\nAs soon as one of the conditions controlling the if is true, the statement associated with that if is executed, and the rest of the C else-if ladder is bypassed.\n\nIf none of the conditions are true, then the final else statement will be executed.';
+  String lifelseSnippet='if ( condition )\n\t\t\t//statements to be executed;\n\nelse if ( condition )\n\t\t\t//statements to be executed;\n.\n.\n.\nelse\n\t\t\t//statements to be executed; ';
 
   @override
   Widget build(BuildContext context){
@@ -28,8 +28,8 @@ class Ifelse extends StatelessWidget{
         children: <Widget>[
          InfoCard(ifStatement,ifDes,ifSnippet),
          InfoCard(ifelseStatement,ifelseDes,ifelseSnippet),
-         InfoCard(NifStatement, NifelseDes, NifelseSnippet),
-         InfoCard(LifStatement, LifelseDes, LifelseSnippet),
+         InfoCard(nifStatement, nifelseDes, nifelseSnippet),
+         InfoCard(lifStatement, lifelseDes, lifelseSnippet),
           SizedBox(
             height: 110.0,
           ),
