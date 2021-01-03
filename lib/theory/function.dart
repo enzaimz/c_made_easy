a612a07ec0ecdfa8ff130b7c62f8f661b6acde1f
amitpola <61548277+amitpola@users.noreply.github.com>
2021-01-03 16:25:42 +0530

    updated functions

--- lib/theory/function.dart
+++ lib/theory/function.dart
@@ -6,7 +6,7 @@ class Functions extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Functions"),
+        title: Hero(tag: 'function',child: Text("Functions",style: TextStyle(fontFamily: 'Audiowide',color: Color(0xFF00FF00) ),)),
       ),
       body: ListView(
         children: <Widget>[
