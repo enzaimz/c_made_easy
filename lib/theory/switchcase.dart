ede8b132e4848f6187c34c4fc1b1e769a31260e6
amitpola <61548277+amitpola@users.noreply.github.com>
2021-01-03 16:30:56 +0530

    updated switch case

--- lib/theory/switchcase.dart
+++ lib/theory/switchcase.dart
@@ -51,7 +51,7 @@ class SwitchCase extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("switch..case"),
+        title: Hero(tag: 'sc',child: Text("switch..case",style: TextStyle(fontFamily: 'Audiowide',color: Color(0xFF00FF00) ),)),
       ),
       body: ListView(
         children: <Widget>[
